3fe34c62bb45a0a13b9be8df6cff7d3799681ca3
Kien Nguyen <ryan@exgo.vn>
2019-07-27 00:21:24 +0700

    hide keyboard

--- app/src/main/java/com/c4f/firebase_chat/MainActivity.java
+++ app/src/main/java/com/c4f/firebase_chat/MainActivity.java
@@ -1,7 +1,9 @@
 package com.c4f.firebase_chat;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -57,8 +59,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         if (getFragmentManager().getBackStackEntryCount() == 0) {
+            hideKeyboard(this);
             getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         }
         super.onBackPressed();
+    }
+
+    public void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
